2816913a8d711f79c4933a4f9c1619a6e44e42b7
kazhesman <kazhesman@gmail.com>
2022-10-20 23:13:17 +0300

    fix

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 $n = 10;
+// $sum = 0;
 function sum_row($n) {
+    global $sum;
     for ($i=0; $i < $n; $i++) { 
         echo $i * $i;
         echo "</br>\n";
+        $sum += $i * $i;
     }
+    return $sum;
 }
 sum_row($n);
+echo "$sum";
